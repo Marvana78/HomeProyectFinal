27706239b42a2dfd626b4f21f6d56a313082b767
Silvana <silvanaterraza@hotmail.com>
2023-09-22 23:15:29 -0300

    trabajando en rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Navbar } from "./Components/Navbar";
 import { Footer } from "./Components/Footer";
 import style from './css/style.css';
 import Logo from './assets/img/logo1.png';
-import { Approuter } from "./router/Approuter";
 
 
 
 function App() {
   return (
   <>
-   <Approuter/>
+   
     <Navbar/>
     
     <div className="imagenlogo">
--- src/router/Approuter.jsx
+++ src/router/Approuter.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Route, Router } from 'react-router-dom';
 
 export const Approuter = () => {
   return (
-    <>
-   
-
-
-    </>
+    <div>Approuter</div>
   )
 }
