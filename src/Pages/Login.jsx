e0fde568eece7f8eb78c78e33bd60174f769c674
Agüero Hernan <zurdograficos@gmail.com>
2023-10-05 21:03:45 -0300

    Creando el front de login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 
+
 export const Login = () => {
-  return (
-    <>
-    <Navbar/>
-    <h1>Login</h1>
-    <Footer/>
-    </>
-  )
-}
+  	const [email, setEmail] = useState('');
+		const [password, setPassword] = useState('');
+	
+    const handleSubmit = (e) => {
+      e.preventDefault
+    }
+  
+    if(email === '' || password === '') {
+      return console.log('Todos los campos son obligatorios');
+    }
+
+    return (
+		<>
+			<Navbar />
+			<h1 className='container text-center text-primary'>Iniciar Sesion</h1>
+
+			<div>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group className='mt-4 ' controlId='nombre'>
+						<Form.Label>Email</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Ingrese su nombre'
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+					</Form.Group>
+
+					<Form.Group className='mt-2' controlId='edad'>
+						<Form.Label>Contraseña</Form.Label>
+						<Form.Control
+							type='number'
+							id='Ingrese su password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</Form.Group>
+
+					<Button className='mt-5 w-100 p-2' variant='primary' type='submit'>
+						Iniciar Sesion
+					</Button>
+				</Form>
+			</div>
+			<Footer />
+		</>
+	);
+};
 export default Login;
