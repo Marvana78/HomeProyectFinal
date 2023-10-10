5c1b34a2e12f507c01e58775fb2873b18fdf4572
Daniela Artaza Quiroga <danibierber@gmail.com>
2023-10-10 02:29:31 -0300

    agrego boton de enviar pedidos y estilos

--- src/Pages/Pedidos.jsx
+++ src/Pages/Pedidos.jsx
@@ -4,12 +4,11 @@ import Footer from '../Components/Footer';
 import serverAPI from '../api/serverAPI';
 import '../css/style.css';
 
-
-
-
 const Pedidos = () => {
   const [menus, setMenus] = useState([]);
   const [costoTotal, setCostoTotal] = useState(0);
+  const [pedidoGuardado, setPedidoGuardado] = useState(false);
+  const [mensajeConfirmacion, setMensajeConfirmacion] = useState('');
 
   useEffect(() => {
     serverAPI.get('/menu/GetMenu')
@@ -22,26 +21,40 @@ const Pedidos = () => {
       })
       .catch((error) => {
         console.error('Error al obtener los menús:', error);
-      
       });
   }, []);
-  console.log(menus);
+
+  // Función para simular el pedido sin guardar en la base de datos
+  const hacerPedido = () => {
+    // Aquí puedes simular el proceso de guardar el pedido
+    // Por ejemplo, puedes establecer una variable de estado para indicar que el pedido se ha enviado
+    setPedidoGuardado(true);
+    setMensajeConfirmacion('Pedido pendiente'); // Mensaje de confirmación simulado
+  };
+
   return (
     <div>
-      <Navbar/>
-      <h1 className='textcenter'>Mis pedidos</h1>
-      <ul>
+      <Navbar />
+      <h1 style={{ textAlign: 'center', color: 'black' }}>Mis pedidos</h1>
+      <ul style={{ listStyleType: 'none' }}>
         {menus.map((menu) => (
-          <li key={menu._id}>
-            <p>Nombre: {menu.Nombre}</p>
-            <p>Descripción: {menu.Descripcion}</p>
-            <p>Monto: {menu.Monto}</p>
-            <p>Unidades: {menu.Unidades}</p>
+          <li key={menu._id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+            <p style={{ color: ' #f57c00' }}>Nombre: {menu.Nombre}</p>
+            <p style={{ color: ' #f57c00' }}>Descripción: {menu.Descripcion}</p>
+            <p style={{ color: ' #f57c00' }}>Monto: ${menu.Monto}</p>
+            <p style={{ color: ' #f57c00' }}>Unidades: {menu.Unidades}</p>
           </li>
         ))}
       </ul>
-      <p>Costo Total: ${costoTotal}</p>
-      <Footer/> 
+      <p style={{ fontWeight: 'bold', fontSize: '18px', color: 'black' }}>Costo Total: ${costoTotal}</p>
+      {pedidoGuardado && <p style={{ color: 'green' }}>{mensajeConfirmacion}</p>}
+      {!pedidoGuardado && (
+        <div>
+          <button onClick={hacerPedido}>Enviar Pedido</button>
+          {mensajeConfirmacion && <p style={{ color: 'red' }}>{mensajeConfirmacion}</p>}
+        </div>
+      )}
+      <Footer />
     </div>
   );
 };
