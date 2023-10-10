71dcd160515c639f455d37320c6ff8f09c437d9f
Daniela Artaza Quiroga <danibierber@gmail.com>
2023-10-10 00:43:14 -0300

    renderizando productos a pedidos

--- src/Pages/Pedidos.jsx
+++ src/Pages/Pedidos.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import NavBar from '../Components/Navbar';
+import React, { useState, useEffect } from 'react';
+import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import serverAPI from '../api/serverAPI';
+import '../css/style.css';
 
 
-export const Pedidos = () => {
-  
+
+
+const Pedidos = () => {
+  const [menus, setMenus] = useState([]);
+  const [costoTotal, setCostoTotal] = useState(0);
+
+  useEffect(() => {
+    serverAPI.get('/menu/GetMenu')
+      .then((response) => {
+        setMenus(response.data);
+
+        // Calcular el costo total
+        const total = response.data.reduce((acc, menu) => acc + menu.Monto, 0);
+        setCostoTotal(total);
+      })
+      .catch((error) => {
+        console.error('Error al obtener los menús:', error);
+      
+      });
+  }, []);
+  console.log(menus);
   return (
-    <>
-    <NavBar/>
-  
-   <h1>Pedidos</h1>
+    <div>
+      <Navbar/>
+      <h1 className='textcenter'>Mis pedidos</h1>
+      <ul>
+        {menus.map((menu) => (
+          <li key={menu._id}>
+            <p>Nombre: {menu.Nombre}</p>
+            <p>Descripción: {menu.Descripcion}</p>
+            <p>Monto: {menu.Monto}</p>
+            <p>Unidades: {menu.Unidades}</p>
+          </li>
+        ))}
+      </ul>
+      <p>Costo Total: ${costoTotal}</p>
+      <Footer/> 
+    </div>
+  );
+};
 
-   <Footer/>
-    </>
-  )
-}
 export default Pedidos;
